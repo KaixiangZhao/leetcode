8960b4cb8a8fceecacc9543f6c7fe9c9e75fbc92
KaixiangZhao <michaelzhao826@gmail.com>
2016-04-20 17:20:14 +0800

    April 20th Update

---
+++ src/algorithm/No236.java
@@ -0,0 +1,18 @@
+package algorithm;
+
+import datastructure.TreeNode;
+
+public class No236 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root==null) return null;
+        if(root==p) return root;
+        if(root==q) return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if(left!=null&&right!=null){
+            return root;
+        }else{
+            return left==null?right:left;
+        }
+    }
+}
